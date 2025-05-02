bfe7c7b8ba4cf9dc9cf0fc01c9299898ebf90ee1
adhilna <adhilchy11@gmail.com>
2025-05-02 10:59:11 +0530

    Add authentication to ServiceListView with IsAuthenticated

--- backend/services/views.py
+++ backend/services/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 from .models import Service
 from .serializers import ServiceSerialzer
+from rest_framework.permissions import IsAuthenticated
 
 class ServiceListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Service.objects.all()
     serializer_class = ServiceSerialzer
 
     def get_queryset(self):
+        queryset = super().get_queryset()
         location = self.request.query_params.get('location')
         if location:
             return self.queryset.filter(worker__workerprofile__location=location)
-        return self.queryset
+        return queryset
     
